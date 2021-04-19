344757c2496a60c0dc02143e3fa512e021fa3835
takeshi-kishima <yahoo.fukuoka@gmail.com>
2021-04-19 22:56:56 +0900

    マテリアルUIボタンを追加して、APIサーバからJson取得しただけ

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import useReactRouter from 'use-react-router';
+import Button from '@material-ui/core/Button';
+
 import logo from './logo.svg';
 import './App.css';
+
+function Hello() {
+  const { history, location, match } = useReactRouter();
+  return (
+    <div>
+      <h1>HelloWorld</h1>
+      <p>{`pathname: ${location.pathname}`}</p>
+      <Button color="primary" variant="contained" onClick={() => history.push('/react')}>Next</Button>
+    </div>
+  );
+}
+
+function HelloReact() {
+  const { history, location, match } = useReactRouter();
+  return (
+    <div>
+      <h1>HelloReact</h1>
+      <p>{`pathname: ${location.pathname}`}</p>
+      <Button color="secondary" variant="contained" onClick={() => history.push('/')}>Next</Button>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -18,9 +44,24 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" onClick={() => {console.log('↓'); waiwai(); console.log('↑');}}>xxxx</Button>
+        <Router>
+          <Switch>
+            <Route path="/" component={Hello} exact />
+            <Route path="/react" component={HelloReact} exact />
+          </Switch>
+        </Router>
       </header>
     </div>
   );
 }
 
+function waiwai() {
+  fetch("http://localhost:8080/greeting", 
+  {
+    mode: 'cors'
+  }).then((res) => res.json()).then(json => {
+    console.log(json);
+  });
+}
 export default App;
